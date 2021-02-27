55725ec997daa7055e4940c8fe3230ce80f250fc
Cassio Leodegario <cassio.leodegario@outlook.com>
2021-02-27 07:44:19 -0400

    feat: hr-worker implementation

---
+++ hr-worker/src/main/java/com/simplemicroservice/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.simplemicroservice.hrworker.repositories;
+
+import com.simplemicroservice.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
